b98803c5e9913b337e1f7ebbb64d0febcc402674
SteadFast02 <achal02gupta@gmail.com>
2025-02-01 23:36:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import ClearUp from "./components/useeffect/ClearnUp";
+// import UseEffect from "./components/useeffect/UseEffect";
+// import UseState from "./components/usestate/UseState";
+import ReducerHook from "./components/usereducer/ReducerHook";
+import UseState from "./components/usestate/UseState";
+// import Parent from "./Parent";
+// import RefHook from "./components/useref/RefHook";
+// import RefHook1 from "./components/useref/RefHook1";
+// import LayoutEffect from "./components/uselayouteffect/LayoutEffect";
+// import MemoHook from "./components/usememo/MemoHook";
+// import Callbackhook from "./components/usecallback/CallbackHook";
+
+const App = () => {
+  // return <ReducerHook />;
+  return <UseState />;
+};
+
+export default App;
